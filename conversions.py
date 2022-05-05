0582b1232166ed3817456779c6483924c25743e9
Brian Smith <Brianmatthewsmith33@outlook.com>
2022-05-05 16:35:01 -0700

    second commit

---
+++ conversions.py
@@ -0,0 +1,38 @@
+def Fahrenheit_to_Celsius(F):
+    return (5 / 9) * (F - 32)
+
+
+def Celsius_to_Fahrenheit(C):
+    return ((9 / 5) * C) + 32
+
+
+def Kelvin_to_Fahrenheit(K):
+    return (9 / 5) * (K - 273) + 32
+
+
+def Celsius_to_Kelvin(C):
+    return C + 273
+
+
+def Kelvin_to_Celsius(K):
+    return K - 273
+
+
+def Pounds_to_Grams(lbs):
+    return lbs * 453.59237
+
+
+def Grams_to_Pounds(g):
+    return g / 453.59237
+
+
+def Gallons_to_liters(G):
+    return G * 3.785411784
+
+
+def Liters_to_Gallons(L):
+    return L / 3.785411784
+
+
+def Fahrenheit_to_Kelvin(F):
+    return (5 / 9) * (F - 32) + 273
